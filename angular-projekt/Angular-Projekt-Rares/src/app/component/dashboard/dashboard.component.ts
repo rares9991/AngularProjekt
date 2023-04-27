58d5a153a45907d8f90e2ec08f79d2be7267543a
rares9991 <rares.matisan@stud.ubbcluj.ro>
2023-04-28 02:12:11 +0300

    dashboard done

--- angular-projekt/Angular-Projekt-Rares/src/app/component/dashboard/dashboard.component.ts
+++ angular-projekt/Angular-Projekt-Rares/src/app/component/dashboard/dashboard.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  constructor(private auth : AuthService){ }
 
+  ngOnInit(): void {
+    
+  }
+
+  register(){
+    this.auth.logout();
+  }
 }
