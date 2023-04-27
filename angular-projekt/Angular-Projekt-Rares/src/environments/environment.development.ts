e4cced57d918a0e87a7ebc3d3352f6f3a2fba60e
rares9991 <rares.matisan@stud.ubbcluj.ro>
2023-04-28 01:55:03 +0300

    login and register

---
+++ angular-projekt/Angular-Projekt-Rares/src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-projekt-47e08',
+    appId: '1:556788906949:web:bce2a6c5c83e65e1b5e9ac',
+    storageBucket: 'angular-projekt-47e08.appspot.com',
+    apiKey: 'AIzaSyBY3wo3R_JaD35xov76zt-Yxxc5HG3isj4',
+    authDomain: 'angular-projekt-47e08.firebaseapp.com',
+    messagingSenderId: '556788906949',
+  },};
---
+++ angular-projekt/Angular-Projekt-Rares/src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  firebase: {
+    projectId: 'angular-projekt-47e08',
+    appId: '1:556788906949:web:bce2a6c5c83e65e1b5e9ac',
+    storageBucket: 'angular-projekt-47e08.appspot.com',
+    apiKey: 'AIzaSyBY3wo3R_JaD35xov76zt-Yxxc5HG3isj4',
+    authDomain: 'angular-projekt-47e08.firebaseapp.com',
+    messagingSenderId: '556788906949',
+  },};
